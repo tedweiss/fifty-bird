1746d6a3783a6ca50a9ee27e7f83631146c1bf9c
Ted Weiss <theodore_weiss@outlook.com>
2020-05-04 23:09:51 -0400

    Make pipe gaps slightly random

--- classes/PipePair.lua
+++ classes/PipePair.lua
@@ -11,7 +11,7 @@
 PipePair = Class{}
 
 -- size of the gap between pipes
-local GAP_HEIGHT = 90
+local GAP_HEIGHT = math.random(90, 140)
 
 function PipePair:init(y)
     -- flag to hold whether this pair has been scored (jumped through)
